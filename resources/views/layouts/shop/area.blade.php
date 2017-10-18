3e6dc241e4031106cf61c8e8771d14e6c5bb050a
WaitMoonMan <1033404553@qq.com>
2017-10-18 18:49:21 +0800

    :memo: footer Localization of footer text`

--- resources/views/layouts/shop/area.blade.php
+++ resources/views/layouts/shop/area.blade.php
@@ -5,19 +5,19 @@
                 <div class="row row-tb-20">
                     <div class="footer-col col-sm-6">
                         <div class="footer-about">
-                            <img class="mb-40" src="assets/images/logo_light.png" width="250" alt="">
-                            <p class="color-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dolores quidem mollitia id ipsa nisi necessitatibus iure repudiandae aperiam, odit ipsam dolor fugiat corporis nesciunt illo nemo minus.</p>
+                            <img class="mb-40" src="{{ asset('assets/images/logo_light.png') }}" width="250" alt="">
+                            <p class="color-light">乱数假文悲哀坐特,  伊洛尼莫减体.</p>
                         </div>
                     </div>
                     <div class="footer-col col-sm-6">
                         <div class="footer-top-twitter">
-                            <h2 class="color-lighter">Twitter Feed</h2>
+                            <h2 class="color-lighter">推特饲料</h2>
                             <ul class="twitter-list">
                                 <li class="single-twitter">
-                                    <p class="color-light"><i class="ico fa fa-twitter"></i><a href="#">@masum_rana :</a> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore in reprehenderit.</p>
+                                    <p class="color-light"><i class="ico fa fa-twitter"></i><a href="#">@masum_rana :</a>最好的商城</p>
                                 </li>
                                 <li class="single-twitter">
-                                    <p class="color-light"><i class="ico fa fa-twitter"></i><a href="#">@masum_rana :</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione id corrupti iusto cupiditate omnis.</p>
+                                    <p class="color-light"><i class="ico fa fa-twitter"></i><a href="#">@masum_rana :</a>这是一个很好的商城</p>
                                 </li>
                             </ul>
                         </div>
@@ -28,17 +28,17 @@
                 <div class="row row-tb-20">
                     <div class="footer-col col-sm-6">
                         <div class="footer-links">
-                            <h2 class="color-lighter">Quick Links</h2>
+                            <h2 class="color-lighter">快速链接</h2>
                             <ul>
-                                <li><a href="deals_grid.html">Latest Deals</a>
+                                <li><a href="deals_grid.html">新的交易</a>
                                 </li>
-                                <li><a href="coupons_grid.html">Newest Coupons</a>
+                                <li><a href="coupons_grid.html">最新的优惠券</a>
                                 </li>
-                                <li><a href="contact_us_02.html">Contact Us</a>
+                                <li><a href="contact_us_02.html">联系我们</a>
                                 </li>
-                                <li><a href="404.html">Error 404</a>
+                                <li><a href="404.html">错误 404</a>
                                 </li>
-                                <li><a href="terms_conditions.html">Terms of Use</a>
+                                <li><a href="terms_conditions.html">使用条款</a>
                                 </li>
                                 <li><a href="faq.html">FAQs</a>
                                 </li>
@@ -52,32 +52,32 @@
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_01.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_01.jpg') }}" alt="">
                                 </div>
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_02.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_02.jpg') }}" alt="">
                                 </div>
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_03.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_03.jpg') }}" alt="">
                                 </div>
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_04.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_04.jpg') }}" alt="">
                                 </div>
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_05.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_05.jpg') }}" alt="">
                                 </div>
 
 
                                 <div class="instagram-widget__item col-xs-4">
-                                    <img src="assets/images/instagram/instagram_06.jpg" alt="">
+                                    <img src="{{ asset('assets/images/instagram/instagram_06.jpg') }}" alt="">
                                 </div>
 
 
@@ -88,11 +88,11 @@
             </div>
             <div class="col-xs-12">
                 <div class="payment-methods t-center">
-                    <span><img src="assets/images/icons/payment/paypal.jpg" alt=""></span>
-                    <span><img src="assets/images/icons/payment/visa.jpg" alt=""></span>
-                    <span><img src="assets/images/icons/payment/mastercard.jpg" alt=""></span>
-                    <span><img src="assets/images/icons/payment/discover.jpg" alt=""></span>
-                    <span><img src="assets/images/icons/payment/american.jpg" alt=""></span>
+                    <span><img src="{{ asset('assets/images/icons/payment/paypal.jpg') }}" alt=""></span>
+                    <span><img src="{{ asset('assets/images/icons/payment/visa.jpg') }}" alt=""></span>
+                    <span><img src="{{ asset('assets/images/icons/payment/mastercard.jpg') }}" alt=""></span>
+                    <span><img src="{{ asset('assets/images/icons/payment/discover.jpg') }}" alt=""></span>
+                    <span><img src="{{ asset('assets/images/icons/payment/american.jpg') }}" alt=""></span>
                 </div>
             </div>
         </div>
