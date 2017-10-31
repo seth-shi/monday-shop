8f06d71a74929beb939acc5bf316c983a1ab2847
WaitMoonMan <1033404553@qq.com>
2017-10-31 23:12:02 +0800

    :sparkles: product create `

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Webpatser\Uuid\Uuid;
 
 class ProductController extends Controller
 {
@@ -28,7 +29,15 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request)
     {
-        dd($request->all());
+        // product table field
+        $product = $request->only(['category_id', 'name', 'price', 'price_original']);
+        // product thumb use image list first
+        $product['thumb'] = $request->input('image')[0];
+        $product['uuid'] = Uuid::generate()->hex;
+
+        Product::create($product);
+
+        dd($product, $request->all());
     }
 
     public function show(Product $product)
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['category_id', 'name', 'price', 'price_original', 'thumb', 'uuid'];
 }
