d0421bf5ed2c6b03de3048c5b8e5bb6b250c9892
WaitMoonMan <1033404553@qq.com>
2017-11-16 13:29:35 +0800

    :sparkles: user setting page

--- resources/views/common/user/menu.blade.php
+++ resources/views/common/user/menu.blade.php
@@ -4,10 +4,9 @@
             <a href="{{ url('/user') }}">个人中心</a>
         </li>
         <li class="person">
-            <a href="{{ url('/user') }}">个人资料</a>
             <ul>
-                <li> <a href="{{ url('/user') }}">个人信息</a></li>
-                <li> <a href="{{ url('/user/setting') }}">安全设置</a></li>
+                <li> <a href="{{ url('/user/setting') }}">个人信息</a></li>
+                <li> <a href="{{ url('/user/setting') }}">修改密码</a></li>
                 <li> <a href="{{ url('/user/addresses') }}">收货地址</a></li>
             </ul>
         </li>
