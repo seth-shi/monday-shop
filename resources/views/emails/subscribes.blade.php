a1e52928151805bded929e2554dba321b6dd1043
WaitMoonMan <1033404553@qq.com>
2017-11-17 17:12:30 +0800

    :hammer: emails view add products name

--- resources/views/emails/subscribes.blade.php
+++ resources/views/emails/subscribes.blade.php
@@ -1,17 +1,20 @@
 @component('mail::message')
 # 本周最受欢迎的商品
 ![$likest->name]({{ asset('/storage/' . $likest->thumb) }})
-### {!! $likest->title !!}
+### $likest->name
+> {!! $likest->title !!}
 [查看详情]({{ url("/home/products/{$likest->id}") }})
 ****
 # 本周最好卖的商品
 ![$hotest->name]({{ asset('/storage/' . $hotest->thumb) }})
-### {!! $hotest->title !!}
+### $hotest->name
+> {!! $hotest->title !!}
 [查看详情]({{ url("/home/products/{$hotest->id}") }})
 ****
 # 本周最新的商品
 ![$latest->name]({{ asset('/storage/' . $latest->thumb) }})
-### {!! $latest->title !!}
+### $latest->name
+> {!! $latest->title !!}
 [查看详情]({{ url("/home/products/{$latest->id}") }})
 ****
 
