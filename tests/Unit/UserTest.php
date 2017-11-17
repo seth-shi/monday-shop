beeaea561f4defabebf2424ce7d17d88d21656b4
WaitMoonMan <1033404553@qq.com>
2017-11-17 21:53:05 +0800

    :white_check_mark: test Database has

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
