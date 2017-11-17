a250fc2691fd38cfa5b8f93c99e38e06b8083e7b
WaitMoonMan <1033404553@qq.com>
2017-11-17 17:15:38 +0800

    :hammer: emails products

--- resources/views/emails/subscribes.blade.php
+++ resources/views/emails/subscribes.blade.php
@@ -1,19 +1,19 @@
 @component('mail::message')
 # 本周最受欢迎的商品
 ![$likest->name]({{ asset('/storage/' . $likest->thumb) }})
-### $likest->name
+### {{ $likest->name }}
 > {!! $likest->title !!}
 [查看详情]({{ url("/home/products/{$likest->id}") }})
 ****
 # 本周最好卖的商品
 ![$hotest->name]({{ asset('/storage/' . $hotest->thumb) }})
-### $hotest->name
+### {{ $hotest->name }}
 > {!! $hotest->title !!}
 [查看详情]({{ url("/home/products/{$hotest->id}") }})
 ****
 # 本周最新的商品
 ![$latest->name]({{ asset('/storage/' . $latest->thumb) }})
-### $latest->name
+### {{ $latest->name }}
 > {!! $latest->title !!}
 [查看详情]({{ url("/home/products/{$latest->id}") }})
 ****
