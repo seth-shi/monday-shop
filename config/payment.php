f23653bc615db4e681e5e692bb13d08731212e56
WaitMoonMan <1033404553@qq.com>
2017-11-20 18:11:13 +0800

    :hammer: config change .env

--- config/payment.php
+++ config/payment.php
@@ -3,6 +3,6 @@
     return [
       'uid' => env('PAYSAPI_UID'),
       'token' => env('PAYSAPI_TOKEN'),
-      'notify_url' => url('/user/pay/notify'),
-      'return_url' => url('/user/pay/return'),
+      'notify_url' => env('PAYSAPI_NOTIFY_URL'),
+      'return_url' => env('PAYSAPI_RETURN_URL'),
     ];
