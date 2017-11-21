4666e2120e28d574f5c81b6bcc8e318746c80330
WaitMoonMan <1033404553@qq.com>
2017-11-21 21:11:22 +0800

    :hammer: payment`s result page

--- resources/views/user/payments/index.blade.php
+++ resources/views/user/payments/index.blade.php
@@ -110,30 +110,8 @@
 	<div class="clear"></div>
 </div>
 </div>
-<div class="footer">
-	<div class="footer-hd">
-		<p>
-			<a href="#">恒望科技</a>
-			<b>|</b>
-			<a href="#">商城首页</a>
-			<b>|</b>
-			<a href="#">支付宝</a>
-			<b>|</b>
-			<a href="#">物流</a>
-		</p>
-	</div>
-	<div class="footer-bd">
-		<p>
-			<a href="#">关于恒望</a>
-			<a href="#">合作伙伴</a>
-			<a href="#">联系我们</a>
-			<a href="#">网站地图</a>
-			<em>© 2015-2025 Hengwang.com 版权所有</em>
-		</p>
-	</div>
-</div>
-</div>
 
+@include('common.user.footer')
 
 <div class="clear"></div>
 
---
+++ resources/views/user/payments/result.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 ,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>结算页面</title>
+
+    <link href="{{ asset('assets/user/AmazeUI-2.4.2/assets/css/amazeui.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/user/AmazeUI-2.4.2/assets/css/admin.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/user/basic/css/demo.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/user/css/jsstyle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/user/css//sustyle.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+
+<!--顶部导航条 -->
+@include('common.user.header')
+
+<!--悬浮搜索框-->
+
+
+<div class="take-delivery">
+ <div class="status">
+     @if ($payment)
+        <h2><img src="{{ asset('images/success.jpg') }}">您已成功付款</h2>
+        <div class="successInfo">
+         <ul>
+           <li>付款金额：<em> {{ $payment->price }}</em></li>
+           <div class="user-info">
+             <p>商品名：{{ $payment->goodsname }}</p>
+           </div>
+         </ul>
+        </div>
+     @else
+         <h2><img src="{{ asset('images/error.jpg') }}">支付失败</h2>
+         <div class="successInfo">
+             请稍后刷新再次尝试
+         </div>
+     @endif
+  </div>
+</div>
+
+@include('common.user.footer')
+
+
+</body>
+</html>
