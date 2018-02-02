259bc03e7b3cec9c12598161bfcf0aa70bb1a73b
DavidNineRoc <1033404553@qq.com>
2018-02-02 21:31:50 +0800

    '正确的引用模型继承的命名空间'

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Car extends Model
 {
     protected $table = 'cars';
--- app/Models/Subscribe.php
+++ app/Models/Subscribe.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Subscribe extends Model
 {
     protected $table = 'subscribes';
