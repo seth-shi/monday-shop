f7e874edd047a674bb213f0b264ab428d364b32d
david <1033404553@qq.com>
2018-10-20 12:50:15 +0800

    订单填充开始处理

---
+++ database/seeds/OrdersSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class OrdersSeeder extends Seeder
+{
+
+    public function run()
+    {  if (! function_exists('ceilTwoPrice')) {
+        require admin_path('helpers.php');
+    }
+
+
+        for ($k = 0; $k < 9; ++ $k) {
+            /**
+             * @var $user User
+             * @var $address Address
+             * @var $product Product
+             */
+            $user = User::query()->inRandomOrder()->first();
+            $address = $user->addresses()->first();
+            $address = $address->format();
+            $user_id = $user->getKey();
+
+            $masterOrder = new Order(compact('user_id', 'address'));
+
+
+            // 随机几个明细
+            $data = [];
+            for ($i = 0, $l = mt_rand(1, 4); $i < $l; ++ $i) {
+
+                $product = Product::query()->inRandomOrder()->first();
+
+                $numbers = mt_rand(1, 9);
+                $price = $product->price;
+                $total = ceilTwoPrice($price * $numbers);
+                $product_id = $product->getKey();
+
+                $masterOrder->total += $total;
+                $data[] = compact('numbers', 'price', 'total', 'product_id');
+            }
+
+
+            $masterOrder->save();
+            $masterOrder->details()->createMany($data);
+        }
+    }
+}
