59f7b7e0d447fe51d9143a9696102613b268c083
david <1033404553@qq.com>
2018-10-20 11:41:12 +0800

    商品功能完成

---
+++ resources/lang/zh-CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少为六个字符匹配的确认。',
+    'reset' => '您的密码已重置！',
+    'sent' => '我们已经通过电子邮件发送了你的密码重置链接！',
+    'token' => '这个重置的token无效.',
+    'user' => "找不到和这个邮箱对应的用户。",
+
+];
