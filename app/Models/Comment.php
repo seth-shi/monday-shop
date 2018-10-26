a2ba80b7f9d0d4b0051de5edacfb34d4df95c579
david <1033404553@qq.com>
2018-10-26 23:22:32 +0800

    评论完成

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
