cee390c1c09b521bed1ee930b243c90090c2086e
david <1033404553@qq.com>
2018-11-22 12:41:46 +0800

    更新注册人数

--- app/Models/SiteCount.php
+++ app/Models/SiteCount.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteCount extends Model
 {
-    //
+    protected $fillable = [
+        'date', 'register_count', 'github_register_count', 'qq_register_count', 'weibo_register_count',
+        'product_sale_count', 'product_sale_money_count'
+    ];
 }
