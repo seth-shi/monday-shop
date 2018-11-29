a294278f74e48a8957e8cb5e9d8624c2f4cb9a4e
david <1033404553@qq.com>
2018-11-29 17:48:56 +0800

    更改样式

--- resources/views/hint/validate_errors.blade.php
+++ resources/views/hint/validate_errors.blade.php
@@ -1,6 +1,6 @@
 @if (count($errors) > 0)
-    <div class="alert alert-danger" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="alert alert-danger am-alert am-alert-danger" role="alert" data-am-alert>
+        <button type="button" class="close am-close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         {{ $errors->first() }}
     </div>
 @endif
