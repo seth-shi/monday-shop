4a4e0d93bc59bd8c43b371f4bfa7cd2b1821cc19
david <1033404553@qq.com>
2018-12-14 16:09:01 +0800

    邮件中增加价格提醒

--- resources/views/emails/seckills.blade.php
+++ resources/views/emails/seckills.blade.php
@@ -2,7 +2,8 @@
 # 你好！这是推送邮件。
 ****
 你收藏的商品：[{{ $product->name }}]，有秒杀活动了 !!!
-![$product->name]({{ $product->thumb }})
+原价：{{ $product->price }}, 现在只要**{{ $seckill->price }}**
+![{{ $product->name }}]({{ $product->thumb }})
 * 秒杀开始时间：**{{ $seckill->start_at }}**
 [查看详情]({{ url("/seckills/{$seckill->id}") }})
 ****
