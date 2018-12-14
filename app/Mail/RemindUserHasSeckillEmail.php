963b6f7d500ff9a2360b9504b71e0f068a236780
david <1033404553@qq.com>
2018-12-14 16:04:08 +0800

    邮件推送

---
+++ app/Mail/RemindUserHasSeckillEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Product;
+use App\Models\Seckill;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RemindUserHasSeckillEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $seckill;
+    public $product;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Product $product
+     */
+    public function __construct(Seckill $seckill, Product $product)
+    {
+        $this->product = $product;
+        $this->seckill = $seckill;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.seckills');
+    }
+}
---
+++ resources/views/emails/seckills.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# 你好！这是推送邮件。
+****
+你收藏的商品：[{{ $product->name }}]，有秒杀活动了 !!!
+![$product->name]({{ $product->thumb }})
+* 秒杀开始时间：**{{ $seckill->start_at }}**
+[查看详情]({{ url("/seckills/{$seckill->id}") }})
+****
+
+@component('mail::button', ['url' => "/"])
+来星期一商城看看
+@endcomponent
+
+@endcomponent
