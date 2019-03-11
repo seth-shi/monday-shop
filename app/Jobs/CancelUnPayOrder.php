f114f04755b33fcc97bfa7e40eb15f2081fd02e6
david <1033404553@qq.com>
2019-03-11 11:29:56 +0800

    秒杀订单自动取消

--- app/Jobs/CancelUnPayOrder.php
+++ app/Jobs/CancelUnPayOrder.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class CancelUnPayOrder
+class CancelUnPayOrder implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
