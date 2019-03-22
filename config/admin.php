3c642fc4939c696a7768b9fcb46e4e8fff26ce59
david <1033404553@qq.com>
2019-03-22 17:27:06 +0800

    添加登录背景图

--- config/admin.php
+++ config/admin.php
@@ -246,7 +246,7 @@ return [
     | This value is used to set the background image of login page.
     |
     */
-    'login_background_image' => '',
+    'login_background_image' => '/images/background.gif',
 
     /*
     |--------------------------------------------------------------------------
