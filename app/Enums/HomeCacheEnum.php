596b836f5a8a5bbca4e502894a32644908db9731
david <1033404553@qq.com>
2019-04-03 14:08:16 +0800

    更换首页缓存key为枚举

---
+++ app/Enums/HomeCacheEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+
+use MyCLabs\Enum\Enum;
+
+class HomeCacheEnum extends Enum
+{
+    const CATEGORIES = 'home:categories';
+    const HOTTEST = 'home:hottest';
+    const LATEST = 'home:latest';
+    const USERS = 'home:users';
+}
