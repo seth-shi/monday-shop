cc39765d2e4b85fac8af3dfcd2bb61716d3ea9c8
david <1033404553@qq.com>
2019-05-16 16:32:39 +0800

    获取商品列表

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => (string)$this->uuid,
+            'name' => (string)$this->name,
+            'title' => (string)$this->title,
+            'price' => (double)$this->price,
+            'original_price' => (double)$this->original_price,
+            'thumb' => $this->thumb,
+            'sale_count' => (int)$this->sale_count,
+            'count' => (int)$this->count,
+            'view_count' => (int)$this->view_count,
+            'created_at' => (string)$this->created_at,
+
+            'pictures' => $this->when($this->isDetail, $this->assertPictures($this->pictures)),
+        ];
+    }
+
+    protected function assertPictures($pictures)
+    {
+        return collect($pictures)->map(function ($uri) {
+
+            return assertUrl($uri);
+        });
+    }
+}
