9f8015bf1630e336d143cbe8584a9db671018a88
david <1033404553@qq.com>
2019-09-05 08:12:35 +0800

    优惠券的显示

--- app/Http/Controllers/CouponTemplateController.php
+++ app/Http/Controllers/CouponTemplateController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CouponTemplate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CouponTemplateController extends Controller
 {
     public function index()
     {
-        return view('coupons.templates');
+        $today = Carbon::today()->toDateString();
+
+        // 只查询未过期的
+        $templates = CouponTemplate::query()->where('end_date', '>=', $today)->get();
+
+        return view('coupons.templates', compact('templates'));
     }
 }
--- resources/views/coupons/templates.blade.php
+++ resources/views/coupons/templates.blade.php
@@ -5,49 +5,64 @@
 @endsection
 
 @section('main')
+    {{--    <div class="coupon-item">--}}
+    {{--        <div class="style-three have">--}}
+    {{--            <div class="info-box">--}}
+    {{--                <p class="nick">xxx</p>--}}
+    {{--                <div class="coupon-money">--}}
+    {{--                    <div class="lay of">￥<em>10</em></div>--}}
+    {{--                    <div class="lay">--}}
+    {{--                        <p class="tit">优惠?</p>--}}
+    {{--                        <p class="demand">满100元可用</p>--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--            <a href="javascript:;" class="get-btn">--}}
+    {{--                <span>已领取</span>--}}
+    {{--            </a>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
     <main id="mainContent" class="main-content">
         <div class="page-container ptb-60">
             <div class="container">
 
                 @include('hint.status')
+                <div class="row row-masnory row-tb-20">
+                    <div class="coupon-wrapper">
+                        @foreach ($templates as $template)
+                            <div class="col-sm-6 col-lg-4">
 
-                <div class="coupon-wrapper">
-                    <h3>xxx?领取：</h3>
-                    <div class="coupon-item">
-                        <div class="style-three">
-                            <div class="info-box">
-                                <p class="nick">monday优惠券</p>
-                                <div class="coupon-money">
-                                    <div class="lay of">￥<em>10</em></div>
-                                    <div class="lay">
-                                        <p class="tit">满100元可用</p>
-                                        <p class="demand">2019-09-31~2019-09-21</p>
+                                <div class="coupon-item">
+                                    <div class="style-three">
+                                        <div class="info-box">
+                                            <p class="nick">{{ $template->title }}</p>
+                                            <div class="coupon-money">
+                                                <div class="lay of">￥<em>{{ $template->amount }}</em></div>
+                                                <div class="lay">
+                                                    @if ($template->full_amount > 0)
+                                                        <p class="tit">满{{ $template->full_amount }}元可用</p>
+                                                    @else
+                                                        <p class="tit">无门槛</p>
+                                                    @endif
+                                                    <p class="demand"
+                                                       style="color: #FFB800;">{{ $template->start_date }} ~</p>
+                                                    <p class="demand"
+                                                       style="color: #5FB878;">{{ $template->end_date }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a href="javascript:;" class="get-btn">
+                                            <span>点击领取</span>
+                                        </a>
                                     </div>
+
                                 </div>
                             </div>
-                            <a href="javascript:;" class="get-btn">
-                                <span>点击领取</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="coupon-item">
-                        <div class="style-three have">
-                            <div class="info-box">
-                                <p class="nick">xxx</p>
-                                <div class="coupon-money">
-                                    <div class="lay of">￥<em>10</em></div>
-                                    <div class="lay">
-                                        <p class="tit">优惠?</p>
-                                        <p class="demand">满100元可用</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="javascript:;" class="get-btn">
-                                <span>已领取</span>
-                            </a>
-                        </div>
+
+                        @endforeach
                     </div>
                 </div>
+
             </div>
         </div>
     </main>
