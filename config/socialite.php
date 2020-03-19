32ab5aa98d858a495651e8d635757a499c056b8f
davidnineroc <1033404553@qq.com>
2020-03-19 18:09:44 +0800

    修改授权回调地址

--- config/socialite.php
+++ config/socialite.php
@@ -5,16 +5,16 @@ return [
     'github' => [
         'client_id'     => env('OAUTH_GITHUB_ID'),
         'client_secret' => env('OAUTH_GITHUB_SECRET'),
-        'redirect'      => config('app.url') . '/auth/oauth/callback?driver=github',
+        'redirect'      => config('app.url') . '/auth/oauth/callback/github',
     ],
     'qq' => [
         'client_id'     => env('OAUTH_QQ_ID'),
         'client_secret' => env('OAUTH_QQ_SECRET'),
-        'redirect'      => config('app.url') . '/auth/oauth/callback?driver=qq',
+        'redirect'      => config('app.url') . '/auth/oauth/callback/qq',
     ],
     'weibo' => [
         'client_id'     => env('OAUTH_WEIBO_ID'),
         'client_secret' => env('OAUTH_WEIBO_SECRET'),
-        'redirect'      => config('app.url') . '/auth/oauth/callback?driver=weibo',
+        'redirect'      => config('app.url') . '/auth/oauth/callback/weibo',
     ],
 ];
