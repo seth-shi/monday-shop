797b05ac7317f9324e2a393d7b9ceb477ff1b61f
liyang <liyang@buptinfo.com>
2021-10-15 00:32:29 +0800

    删除无用的addresses变量

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -134,7 +134,7 @@ class ProductController extends Controller
             (new ScoreLogServe)->visitedProductAddScore($user, $product);
         }
 
-        return view('products.show', compact('product', 'addresses', 'recommendProducts'));
+        return view('products.show', compact('product', 'recommendProducts'));
     }
 
     /**
