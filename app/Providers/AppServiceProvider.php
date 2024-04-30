e74782b31d029a26cdcaca69fbd951e946376580
seth-shi <1033404553@qq.com>
2024-04-30 11:27:32 +0800

    fix: app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,23 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // 如果在后台运行, 启动后台服务
-        if (request()->is('admin*')) {
-
-            $this->registerEditorService();
-            $this->registerAdminService();
-        } elseif (request()->is('yxx*')) {
-
-            $this->registerEditorService();
-        } elseif (request()->is('api*')) {
-
-            config(['auth.defaults.guard' => 'api']);
-            $this->registerJwtService();
-        } elseif ($this->app->runningInConsole()) {
-
-            Schema::defaultStringLength(191);
-            $this->registerJwtService();
-            $this->registerAdminService();
-        }
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -47,21 +31,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    }
-
-    protected function registerAdminService()
-    {
-        AliasLoader::getInstance()->alias('Admin', Admin::class);
-        $this->app->register(AdminServiceProvider::class);
-    }
-
-    public function registerEditorService()
-    {
-        $this->app->register(EditorProvider::class);
-    }
-
-    protected function registerJwtService()
-    {
-        $this->app->register(LaravelServiceProvider::class);
     }
 }
